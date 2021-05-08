21b9d041c091bf14b74bbbce657188b7c621ba94
Martin Lupa <80256770+MartinLupa@users.noreply.github.com>
2021-05-08 20:24:05 +0200

    Create useStateHook.jsx

---
+++ 13 - React/useStateHook.jsx
@@ -0,0 +1,24 @@
+//REACT HOOKS.
+//useState() and onClick() to listen to events and modify rendering on screen.
+
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
+export default App;
